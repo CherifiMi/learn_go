4e707a65c637b6f018b35d766bd76f5eb665fe89
CherifiMi <cherifi154949@gmail.com>
2024-05-01 04:46:39 +0100

    .

--- hello/hello.go
+++ hello/hello.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf(hello(""))
+	fmt.Printf(hello("", ""))
 }
 
 const hellopre = "hello, "
 
-func hello(s string) string {
+func hello(s string, lang string) string {
 	if s == "" {
 		s = "world"
+	}
+	if lang == "spanish" {
+		return "hola, " + s
 	}
 	return hellopre + s
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -4,13 +4,18 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	t.Run("say hi to name", func(t *testing.T) {
-		g := hello("mito")
+		g := hello("mito", "")
 		w := "hello, mito"
 		assert(t, g, w)
 	})
 	t.Run("say hi to world", func(t *testing.T) {
-		g := hello("")
+		g := hello("", "")
 		w := "hello, world"
+		assert(t, g, w)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		g := hello("marko", "spanish")
+		w := "hola, marko"
 		assert(t, g, w)
 	})
 }
