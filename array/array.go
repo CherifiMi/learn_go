3f62a94ba9c28f4b27a28e03c7f80955bb56a00f
CherifiMi <cherifi154949@gmail.com>
2024-05-02 20:42:52 +0100

    .

--- array/array.go
+++ array/array.go
@@ -9,9 +9,22 @@ func Sum(nums []int) int {
 }
 
 func SumAll(nums ...[]int) []int {
-	sums := make([]int, len(nums))
-	for i, num := range nums {
-		sums[i] = Sum(num)
+	//sums := make([]int, len(nums))
+	var sums []int
+	for _, num := range nums {
+		sums = append(sums, Sum(num))
+	}
+	return sums
+}
+func SumAllTails(nums ...[]int) []int {
+	var sums []int
+	for _, num := range nums {
+		if len(num) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := num[1:]
+			sums = append(sums, Sum(tail))
+		}
 	}
 	return sums
 }
--- array/array_test.go
+++ array/array_test.go
@@ -18,6 +18,20 @@ func TestSumAll(t *testing.T) {
 
 	assert(t, g, w)
 }
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of sume slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		assert(t, got, want)
+	})
+	t.Run("make the sums of empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		assert(t, got, want)
+	})
+}
 
 func assert(t testing.TB, got, want any) {
 	t.Helper()
