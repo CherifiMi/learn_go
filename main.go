06bff5754a6021b46bceb04ee6c47a51cd3a5424
CherifiMi <cherifi154949@gmail.com>
2024-05-03 22:57:34 +0100

    .

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"fmt"
-	"main/integers"
-	strct "main/struct"
 )
 
 func main() {
-	fmt.Println("%d", integers.Add(2, 2))
-	c := strct.Circle{R: 10.0}
-	fmt.Println("", c.Perimeter())
-	fmt.Println("", c.Area())
+	fmt.Println("hello mito")
 }
