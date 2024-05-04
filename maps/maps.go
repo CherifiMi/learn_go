226b083c4b8f26eea45e460602c58addf18c55f2
CherifiMi <cherifi154949@gmail.com>
2024-05-04 13:15:41 +0100

    .

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,19 @@
 package maps
 
+import "errors"
+
 type Dic map[string]string
 
-func (d Dic) Search(s string) string {
-	return d[s]
+func (d Dic) Search(s string) (string, error) {
+	r, ok := d[s]
+
+	if !ok {
+		return "", errors.New("could not find the word")
+	} else {
+		return r, nil
+	}
+}
+
+func (d Dic) Add(key, value string) {
+	d[key] = value
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -7,10 +7,32 @@ import (
 func TestSearch(t *testing.T) {
 	dic := Dic{"test": "111"}
 
-	got := dic.Search("test")
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dic.Search("test")
+		want := "111"
+
+		assert(t, got, want)
+	})
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dic.Search("mito")
+		want := "could not find the word"
+
+		assert(t, err.Error(), want)
+	})
+}
+func TestAdd(t *testing.T) {
+	dic := Dic{}
+	dic.Add("test", "111")
+
+	got, err := dic.Search("test")
 	want := "111"
 
+	if err != nil {
+		t.Fatal("should found the word", err)
+	}
+
 	assert(t, got, want)
+
 }
 
 func assert(t testing.TB, want, got string) {
