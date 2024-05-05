fff9db2d0ef719dd75773a960fdcc90561e735e1
CherifiMi <cherifi154949@gmail.com>
2024-05-05 12:17:18 +0100

    .

--- concurrency/concurrency.go
+++ concurrency/concurrency.go
@@ -1,1 +1,11 @@
 package main
+
+type WebCheker func(string) bool
+
+func CheckWeb(wc WebCheker, urls []string) map[string]bool {
+	result := make(map[string]bool)
+	for _, url := range urls {
+		result[url] = wc(url)
+	}
+	return result
+}
