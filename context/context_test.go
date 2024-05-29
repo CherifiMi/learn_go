a1cbf6aea54266a232a31408aedfd3e9826b4546
CherifiMi <cherifi154949@gmail.com>
2024-05-29 10:39:29 +0100

    .

--- context/context_test.go
+++ context/context_test.go
@@ -1,1 +1,29 @@
 package context
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type SpyStore struct {
+	response string
+}
+
+func (s *SpyStore) Fetch() string {
+	return s.response
+}
+
+func TestServer(t *testing.T) {
+	data := "hello mito"
+	svr := Server(&SpyStore{data})
+
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	res := httptest.NewRecorder()
+
+	svr.ServeHTTP(res, req)
+
+	if res.Body.String() != data {
+		t.Error("err")
+	}
+}
