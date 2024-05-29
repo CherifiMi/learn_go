acd287eb4fe144bb1866afca59437a4827d18b5e
CherifiMi <cherifi154949@gmail.com>
2024-05-29 10:45:15 +0100

    .

--- context/context_test.go
+++ context/context_test.go
@@ -4,26 +4,39 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 type SpyStore struct {
-	response string
+	response  string
+	cancelled bool
+}
+
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
 }
 
 func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
 func TestServer(t *testing.T) {
-	data := "hello mito"
-	svr := Server(&SpyStore{data})
+	t.Run("normal", func(t *testing.T) {
+		data := "hello mito"
+		svr := Server(&SpyStore{data, false})
 
-	req := httptest.NewRequest(http.MethodGet, "/", nil)
-	res := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/", nil)
+		res := httptest.NewRecorder()
 
-	svr.ServeHTTP(res, req)
+		svr.ServeHTTP(res, req)
 
-	if res.Body.String() != data {
-		t.Error("err")
-	}
+		if res.Body.String() != data {
+			t.Error("err")
+		}
+	})
+
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+
+	})
 }
