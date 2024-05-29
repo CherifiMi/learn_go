b2ae6e0489de91db29fa78e344949026d4658d3d
CherifiMi <cherifi154949@gmail.com>
2024-05-29 09:07:24 +0100

    .

--- ref/ref.go
+++ ref/ref.go
@@ -2,12 +2,26 @@ package ref
 
 import "reflect"
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func walk(x interface{}, fn func(string)) {
 	val := reflect.ValueOf(x)
 	for i := 0; i < val.NumField(); i++ {
 		s := val.Field(i)
-		if s.Kind() == reflect.String {
+
+		switch s.Kind() {
+		case reflect.String:
 			fn(s.String())
+		case reflect.Struct:
+			walk(s.Interface(), fn)
 		}
 	}
 }
