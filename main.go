2d4def622ee8e3a7bc0f69e848507720f01451e3
CherifiMi <cherifi154949@gmail.com>
2024-05-31 15:16:43 +0100

    .

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func apiHandler(w http.ResponseWriter, r *http.Request) {
+	response := Response{Message: "Hello, API!"}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
+	http.HandleFunc("/api", apiHandler)
+	http.ListenAndServe(":8080", nil)
 }
