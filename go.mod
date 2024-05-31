c995a73a558fb9f5b348b4ca0cab5d63a36efa7a
CherifiMi <cherifi154949@gmail.com>
2024-05-31 15:25:20 +0100

    .

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module main
 
 go 1.22.2
+
+require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/godbus/dbus/v5 v5.0.3 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
+	github.com/muka/go-bluetooth v0.0.0-20240115085408-dfdf79b8f61d // indirect
+	github.com/sirupsen/logrus v1.6.0 // indirect
+	golang.org/x/sys v0.0.0-20200728102440-3e129f6d46b1 // indirect
+)
--- main.go
+++ main.go
@@ -1,56 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"github.com/muka/go-bluetooth/api"
-	"log"
-)
-
 func main() {
-	// Initialize the Bluetooth adapter
-	manager, err := api.NewManager()
-	if err != nil {
-		log.Fatalf("Failed to initialize Bluetooth manager: %s", err)
-	}
-	defer manager.Close()
 
-	// Start the discovery of nearby Bluetooth devices
-	discovery, cancel, err := manager.Discover()
-	if err != nil {
-		log.Fatalf("Failed to start discovery: %s", err)
-	}
-	defer cancel()
-
-	fmt.Println("Scanning for nearby Bluetooth devices...")
-
-	// Listen for discovered devices
-	go func() {
-		for {
-			select {
-			case ev := <-discovery:
-				if ev.Type == api.DeviceRemoved {
-					// Device removed
-					fmt.Printf("Device removed: %s\n", ev.Path)
-				} else if ev.Type == api.DeviceAdded {
-					// Device added
-					device, err := api.GetDeviceByPath(ev.Path)
-					if err != nil {
-						log.Printf("Failed to get device by path %s: %s", ev.Path, err)
-						continue
-					}
-					props, err := device.GetProperties()
-					if err != nil {
-						log.Printf("Failed to get properties for device %s: %s", device.Path, err)
-						continue
-					}
-					name, _ := props.GetString("Name")
-					address, _ := props.GetString("Address")
-					fmt.Printf("Device added: %s (%s)\n", name, address)
-				}
-			}
-		}
-	}()
-
-	// Wait indefinitely
-	select {}
 }
